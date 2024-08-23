32bfdef042ff336d53d5f86d7b7f70f60c037a84
Sherlock <csanchezcabrera1@gmail.com>
2024-08-22 22:34:13 -0500

    Creacion de la lista, insertar al final y menu

--- ProyectoIntegrador.cpp
+++ ProyectoIntegrador.cpp
@@ -1,37 +1,129 @@
 #include<iostream>
+#include<cstdlib>
+using namespace std;
 
 struct Producto{
-	int id;
-	string nombre;
-	int precio;
-	int unidades;
+    int id;
+    string nombre;
+    int precio;
+    int unidades;
+    Producto* sig;
 };
 
-void crearLista(/*Producto* &cab, int id, string n, int p, int u*/){
-	cout<<"HOLA MUNDO \n";
+void crearLista(Producto* &cab, Producto* &fin, int id, string n, int p, int u){
+    Producto* nuevo = new Producto;
+    nuevo->id = id;
+    nuevo->nombre = n;
+    nuevo->precio = p;
+    nuevo->unidades = u;
+    nuevo->sig = NULL;
+    
+    cab = nuevo;
+    fin = nuevo;
+}
+
+void mostrarLista(Producto* cab){
+    Producto* aux = cab;
+    cout << "===========================INVENTARIO===================== \n";
+    while(aux != NULL){
+        cout << "Id: " << aux->id << "\n";
+        cout << "Nombre: " << aux->nombre << "\n";
+        cout << "Precio: " << aux->precio << "\n";
+        cout << "Unidades: " << aux->unidades << "\n";
+        cout << "----------------------------------------------------------\n";
+        aux = aux->sig;
+    }
+    cout << "\n";
+}
+
+void insertarFinal(Producto* &cab, Producto* &fin, int id, string n, int p, int u){
+    Producto* nuevo = new Producto;
+    
+    nuevo->id = id;
+    nuevo->nombre = n;
+    nuevo->precio = p;
+    nuevo->unidades = u;
+    nuevo->sig = NULL;
+    
+    fin->sig = nuevo;
+    fin = nuevo;    
 }
 
 int main(){
-	cab
-	int op;
-	
-	do{
-		cout<<"1. Insertar producto(s) \n";
-		cout<<"2. Mostrar lista de productos \n";
-		cout<<"3. Actualizar unidades \n";
-		cout<<"4. Eliminar producto(s) \n";
-		cout<<"5. Aplicar descuento \n";
-		cout<<"6. Consultar existencias \n";
-		cout<<"7. Valor total del inventario \n";
-		cout<<"8. Salir \n";
-		cin>>op;
-		
+    Producto* cab = NULL;
+    Producto* fin = NULL;
+    int op, id, p, u, c;
+    string n;
+    bool cond = true;
+
+	while(cond){
+		cout << "===========================MENU===========================\n";
+        cout << "1. Insertar producto(s) \n";
+        cout << "2. Mostrar lista de productos \n";
+        cout << "3. Actualizar unidades \n";
+        cout << "4. Eliminar producto(s) \n";
+        cout << "5. Aplicar descuento \n";
+        cout << "6. Consultar existencias \n";
+        cout << "7. Valor total del inventario \n";
+        cout << "8. Salir \n";
+        cout << "Seleccione una opcion: ";
+        cin >> op;
+        
+        // Manejo de error en la entrada
+        if(cin.fail()){
+            cin.clear(); 
+            cin.ignore(1000, '\n'); 
+            cout << "Entrada no valida. Por favor, ingrese un numero.\n\n";
+            continue; 
+        }
+        
+        cout << "\n";
+        
 		switch(op){
-			case 1:  crearLista();
-			break;
-		}
-	}
-	
-	
-	return 0;
+            
+            case 1:
+                cout << "Cuantos productos desea agregar? \n";
+                cin >> c;
+            
+                for(int i = 1; i <= c; i++){
+                    cout << "-----------------------Producto " << i << "-----------------------\n";
+                    cout << "Ingrese el Id del producto: \n";
+                    cin >> id;
+                    cin.ignore();
+                    
+                    cout << "Ingrese el nombre del producto: \n";
+                    getline(cin, n);
+                    
+                    cout << "Ingrese el precio del producto: \n";
+                    cin >> p;
+                    
+                    cout << "Ingrese las unidades del producto: \n";
+                    cin >> u;
+                    if(cab == NULL){
+                        crearLista(cab, fin, id, n, p, u);
+                    }else{
+                        insertarFinal(cab, fin, id, n, p, u);
+                    }
+                } 
+                
+            break;
+            
+            case 2: 
+                mostrarLista(cab);
+            break;
+            
+            case 8: 
+            	cond = false;
+                cout << "Saliendo del programa...  \n";
+            break;
+            
+            default: 
+                cout << "Opcion no valida, por favor seleccione otra opcion.\n";
+            break;
+        }
+	};
+        
+
+    return 0;
 }
+
